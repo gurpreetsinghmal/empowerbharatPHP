0cfe3a391b7609dac25b6e6cb9113419d4108764
Technominds <harpreetsinghm786@gmail.com>
2023-07-30 15:44:16 +0530

    ok

--- component/meta.php
+++ component/meta.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Empower Bharat - An Initiative to Revive Rural India</title>
+<title><?php echo basename($_SERVER['PHP_SELF'],'.php');?> - Empower Bharat - An Initiative to Revive Rural India</title>
 
 <meta name="description" content="An Initiative to Revive Rural India '', launched on India’s 75th Independence Day with the vision to bridge the Rural-Urban economic divide at the grass-root level by providing quality education for children and generating employment for struggling youth to make them self-reliant. Our model is designed to address the key underlying problems of development through a practical and sustainable process. Poor quality education for young kids would be tackled by providing e-tutorial apps and books and setting up Smart and Unique learning centers in small towns and villages. Trained, motivated youth would be employed here as well. To promptly tackle unemployment on a larger scale, our JobUWant Portal will provide free alerts for all government job vacancies, along with exam strategies and skill development programs. Lastly, we envisage a safe and independent environment for women through safety training, skill development workshops, and most importantly, through our one-of-a-kind STREE Podcast. With Empower Bharat, we strive to make a unified, empowered India a reality.">
 
