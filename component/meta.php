9c36e511ff1b8a6f01906208ee4968c2c7e1331c
Technominds <harpreetsinghm786@gmail.com>
2023-07-30 14:42:45 +0530

    ok

--- component/meta.php
+++ component/meta.php
@@ -1,36 +1,42 @@
 <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Empower Bharat - An Initiative to Revive Rural India</title>
-  
-    <meta name="description" content="An Initiative to Revive Rural India '', launched on India’s 75th Independence Day with the vision to bridge the Rural-Urban economic divide at the grass-root level by providing quality education for children and generating employment for struggling youth to make them self-reliant. Our model is designed to address the key underlying problems of development through a practical and sustainable process. Poor quality education for young kids would be tackled by providing e-tutorial apps and books and setting up Smart and Unique learning centers in small towns and villages. Trained, motivated youth would be employed here as well. To promptly tackle unemployment on a larger scale, our JobUWant Portal will provide free alerts for all government job vacancies, along with exam strategies and skill development programs. Lastly, we envisage a safe and independent environment for women through safety training, skill development workshops, and most importantly, through our one-of-a-kind STREE Podcast. With Empower Bharat, we strive to make a unified, empowered India a reality.">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Empower Bharat - An Initiative to Revive Rural India</title>
 
-    <meta name="keywords" content="Empower Bharat, India, empowerment, community development, education, Rural Inida, Women Safety, Jobs ,Employment">
+<meta name="description" content="An Initiative to Revive Rural India '', launched on India’s 75th Independence Day with the vision to bridge the Rural-Urban economic divide at the grass-root level by providing quality education for children and generating employment for struggling youth to make them self-reliant. Our model is designed to address the key underlying problems of development through a practical and sustainable process. Poor quality education for young kids would be tackled by providing e-tutorial apps and books and setting up Smart and Unique learning centers in small towns and villages. Trained, motivated youth would be employed here as well. To promptly tackle unemployment on a larger scale, our JobUWant Portal will provide free alerts for all government job vacancies, along with exam strategies and skill development programs. Lastly, we envisage a safe and independent environment for women through safety training, skill development workshops, and most importantly, through our one-of-a-kind STREE Podcast. With Empower Bharat, we strive to make a unified, empowered India a reality.">
 
-    <meta name="author" content="Empower Bharat">
-    <meta name="robots" content="index, follow">
-    <link rel="canonical" href="https://www.empowerbharat.in/">
+<meta name="keywords" content="Empower Bharat, India, empowerment, community development, education, Rural Inida, Women Safety, Jobs ,Employment">
 
-    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
-    <style>
-    :root {
-      --primary: #7AC3A3;
-      --secondary: #255F4C;
-      font-family:'poppins';
-    }
+<meta name="author" content="Empower Bharat">
+<meta name="robots" content="index, follow">
+<link rel="canonical" href="https://www.empowerbharat.in/">
 
-    ::-webkit-scrollbar {
-      width: 10px;
-    }
+<!-- If empowerbharat.in has social media sharing functionality, consider using Open Graph meta tags to control how content appears when shared on platforms like Facebook. -->
+<meta property="og:title" content="Empower Bharat | Driving Positive Change in India">
+<meta property="og:description" content="Empower Bharat is dedicated to fostering social initiatives and empowering communities for a brighter future in India. Join us in driving positive change.">
+<meta property="og:image" content="https://www.empowerbharat.in/image.jpg">
+<meta property="og:url" content="https://www.empowerbharat.in">
 
-    ::-webkit-scrollbar-thumb {
-      background-color: var(--primary);
-      border-radius: 10px;
-      border: 3px solid #fff;
-    }
-  
+
+<link rel="icon" type="image/x-icon" href="images/favicon.ico">
+<script src="https://cdn.tailwindcss.com"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
+<style>
+  :root {
+    --primary: #7AC3A3;
+    --secondary: #255F4C;
+    font-family: 'poppins';
+  }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--primary);
+    border-radius: 10px;
+    border: 3px solid #fff;
+  }
 </style>
