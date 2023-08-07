9b2104886601a52e99575be0bc750ec85b3d1748
Technominds <harpreetsinghm786@gmail.com>
2023-08-07 22:30:13 +0530

    k

--- join.php
+++ join.php
@@ -21,7 +21,7 @@
 
                 <div class="w-1/2 h-64 flex flex-col justify-between">
                     <div class="md:text-5xl text-xl font-bold text-green-800">
-                        <div class="mb-4">Empower yourself and build</div>
+                        <div class="md:mb-4">Empower yourself and build</div>
                         <div class="">India by working with us</div></div>
                     <div class="font-bold md:text-3xl tracking-wider mb-2 leading-loose">
                         Be an Empowerer
@@ -39,8 +39,8 @@
     </div>
 
 
-      <!-- partner -->
-      <div class="container mx-auto p-4 my-6">
+    <!-- partner -->
+    <div class="container mx-auto p-4 my-6">
         <div class="my-8">
             <div class="w-full mx-auto">
                 <div class="text-4xl mx-auto  mb-2 text-center">Partner</div>
@@ -53,12 +53,12 @@
         </div>
         <div class="md:flex w-full">
             <div class="w-2/3 mx-auto">
-                <img class="w-1/2 mx-auto" src="images/work/partner icon.png ">
+                <img class="w-3/4 md:w-1/2 mx-auto" src="images/work/partner icon.png ">
             </div>
             <div class="w-full">
                 <div class="text-2xl font-bold text-green-800 my-2">Partner with us. Let’s together and create something
                     meaningful!</div>
-                <div class="tracking-wider   mb-2 text-justify leading-loose">
+                <div class="tracking-wider   mb-2 md:text-justify md:leading-loose">
                     At Empower Bharat, collaboration is at the heart of everything we do. We work together with many organizations and create sustainable solutions and drive meaningful impact in the lives of those in need. We invite like-minded partners to join forces with us, share our vision, and contribute to our collective mission of transforming rural India. Partner with us and be a part of an inspiring journey where collaboration leads to empowerment, and together, we can create a brighter and more prosperous future for everyone.
                 </div>
                 <div class="flex justify-center md:justify-start">
@@ -83,11 +83,11 @@
         </div>
         <div class="md:flex w-full flex-row-reverse">
             <div class="w-2/3 mx-auto">
-                <img class="w-1/2 mx-auto" src="images/work/sponsor icon.png ">
+                <img class="w-3/4 md:w-1/2 mx-auto" src="images/work/sponsor icon.png ">
             </div>
             <div class="w-full">
                 <div class="text-2xl font-bold text-green-800 my-2">Sponsor with Purpose. Join us in making a meaningful difference!</div>
-                <div class="tracking-wider text-justify mb-2 leading-loose">At Empower Bharat, we firmly believe that sponsorship can be a powerful catalyst for transforming lives. By becoming a sponsor, you have the opportunity to play a significant role in empowering communities and driving positive change. Join hands with us as we embark on this transformative journey to create a brighter and more equitable future for all. Together, let's make a lasting impact that uplifts and empowers those in need.
+                <div class="tracking-wider md:text-justify mb-2 md:leading-loose">At Empower Bharat, we firmly believe that sponsorship can be a powerful catalyst for transforming lives. By becoming a sponsor, you have the opportunity to play a significant role in empowering communities and driving positive change. Join hands with us as we embark on this transformative journey to create a brighter and more equitable future for all. Together, let's make a lasting impact that uplifts and empowers those in need.
 
                 </div>
                 <div class="flex justify-center md:justify-start">
@@ -111,12 +111,12 @@
         </div>
         <div class="md:flex w-full">
             <div class="w-2/3 mx-auto">
-                <img class="w-1/2 mx-auto" src="images/work/employee icon.png ">
+                <img class="w-3/4 md:w-1/2 mx-auto" src="images/work/employee icon.png ">
             </div>
             <div class="w-full">
                 <div class="text-2xl font-bold text-green-800 my-2">Empowerment Begins Here. Join our team and become a change-maker!
                 </div>
-                <div class="tracking-wider mb-2 text-justify leading-loose">At Empower Bharat, we are more than just a team – we are a passionate community united by a common goal: to make a tangible difference in the lives of people. As an employee, you'll be a valued member of our dynamic organization, contributing your unique skills and ideas to drive meaningful change and create opportunities for empowerment in rural India. Join us as we nurture a culture of empowerment and work collectively to build a brighter tomorrow.
+                <div class="tracking-wider mb-2 md:text-justify md:leading-loose">At Empower Bharat, we are more than just a team – we are a passionate community united by a common goal: to make a tangible difference in the lives of people. As an employee, you'll be a valued member of our dynamic organization, contributing your unique skills and ideas to drive meaningful change and create opportunities for empowerment in rural India. Join us as we nurture a culture of empowerment and work collectively to build a brighter tomorrow.
                 </div>
                 <div class="flex justify-center md:justify-start">
                     <a target="_blank" href="https://forms.gle/4L2kX7tyU1Nh34Ux7" class="bg-green-800 p-3 text-white rounded-full">Apply Now ></a>
@@ -124,7 +124,6 @@
             </div>
         </div>
     </div>
-
     
     <!-- intgern -->
     <div class="container mx-auto p-4 my-6">
@@ -140,11 +139,11 @@
         </div>
         <div class="md:flex w-full flex-row-reverse">
             <div class="w-2/3 mx-auto">
-                <img class="w-1/2 mx-auto" src="images/work/intern icon.png ">
+                <img class="w-3/4 md:w-1/2 mx-auto" src="images/work/intern icon.png ">
             </div>
             <div class="w-full">
                 <div class="text-2xl font-bold text-green-800 my-2">Be the Catalyst for Change. Join as an intern and ignite transformation!</div>
-                <div class="tracking-wider  text-justify mb-2 leading-loose">Are you ready to be the driving force behind social change? As an intern at Empower Bharat, you'll be stepping into a world of endless possibilities where your actions have a profound impact on rural communities. Join our dynamic and inclusive team, gain valuable experience, and embrace the spirit of transformation as we work towards a more promising future for all. Together, let's ignite the spark of positive change and make a real difference in the lives of those we serve.
+                <div class="tracking-wider  md:text-justify mb-2 md:leading-loose">Are you ready to be the driving force behind social change? As an intern at Empower Bharat, you'll be stepping into a world of endless possibilities where your actions have a profound impact on rural communities. Join our dynamic and inclusive team, gain valuable experience, and embrace the spirit of transformation as we work towards a more promising future for all. Together, let's ignite the spark of positive change and make a real difference in the lives of those we serve.
 
                 </div>
                 <div class="flex justify-center md:justify-start">
