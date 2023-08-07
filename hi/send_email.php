249f5c2465dbc95c7f20e42246d49c137447835f
Technominds <harpreetsinghm786@gmail.com>
2023-08-08 02:21:33 +0530

    ok

--- hi/send_email.php
+++ hi/send_email.php
@@ -6,10 +6,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phone = $_POST["phone"];
     $from= $_POST["from"];
     $subject = "empowerbharat website : ".$_POST["emailSubject"];
-    $message = $_POST["emailMessage"];
-    echo $from;  
-    $headers = "From: ".$from."\r\n";
-    $headers .= "Reply-To: ".$from."\r\n";
+    $message = $_POST["emailMessage"].$from;
+    
+    $headers = "From: test@gmail.com\r\n";
+    $headers .= "Reply-To:test@gmail.com\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=utf-8\r\n";
 
