c50810c6dbd72f20044ff53040d815d5126c3a3c
Technominds <harpreetsinghm786@gmail.com>
2023-08-19 20:37:52 +0530

    ok

--- component/navbar-work.php
+++ component/navbar-work.php
@@ -32,7 +32,10 @@
           </button>
           <!-- Dropdown menu -->
           <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-            <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+          <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+              <li>
+                <a href="work.php" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">SUTRA for Kids</a>
+              </li>
               <li>
                 <a href="work.php" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Job U Want</a>
               </li>
@@ -59,7 +62,7 @@
                     </div>
                   </li> -->
               <li>
-                <a href="work.php" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Stree</a>
+                <a href="work.php" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Stree Safety</a>
               </li>
             </ul>
             <!-- <div class="py-1">
