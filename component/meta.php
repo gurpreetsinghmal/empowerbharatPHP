51fdbb198f12856af7fe1dc6a217539b1dceebbc
Technominds <harpreetsinghm786@gmail.com>
2023-10-14 18:55:03 +0530

    font

--- component/meta.php
+++ component/meta.php
@@ -21,13 +21,13 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
 <style>
   :root {
     --primary: #7AC3A3;
     --secondary: #255F4C;
-    font-family: 'poppins';
+    font-family: 'Montserrat';
   }
 
   ::-webkit-scrollbar {
@@ -39,8 +39,5 @@
     border-radius: 10px;
     border: 3px solid #fff;
   }
-  @font-face {
-            font-family: KrutiDev;
-            src: url(fonts/marathi/k010/k010.woff);
-        }
+  
 </style>
